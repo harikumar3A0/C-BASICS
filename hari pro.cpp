761e7a081515a8c33bce2613f2159ca1a4580dd7
harikumar3A0 <92911175+harikumar3A0@users.noreply.github.com>
2021-10-23 12:34:38 +0530

    Add files via upload

---
+++ hari pro.cpp
@@ -0,0 +1,5 @@
+#include<stdio.h>
+void main()
+{
+	printf("roll no\t20p35a03a0\nname\tvasa hari kumar\nbranch\tmechanical\ncollege\taditya college");
+}
---
+++ hari.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+void main()
+{
+	printf("h\na\nr\ni");
+}
