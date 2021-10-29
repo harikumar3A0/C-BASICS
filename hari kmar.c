6ac2355f69426a19331dedc4d94c04ce3f467189
harikumar3A0 <92911175+harikumar3A0@users.noreply.github.com>
2021-10-29 12:22:17 +0530

    Add files via upload

---
+++ hari kmar.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+    //variable declaration
+    int age;
+    float cgpa;
+    char intial;
+    
+    //intialazation 
+    age=17;
+    cgpa=9.3;
+    intial='v';
+    
+    //output
+    printf("%d\n",age);
+    printf("%f\n",cgpa);
+    printf("%c\n",intial);
+}
