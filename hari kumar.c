6c58f87247e61f54d792597974682b426777a43c
harikumar3A0 <92911175+harikumar3A0@users.noreply.github.com>
2021-11-11 22:24:05 -0800

    Add files via upload

--- hari kumar.c
+++ hari kumar.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
-void main()
+void main ()
 {
-	printf("welcome aditya");
+	int minutes,h,m;
+	printf("enter minutes");
+	scanf("%d",&minutes);
+	h=minutes/60;
+	m=minutes%60;
+	printf("%d hours(s) %d minutes(s)",h,m);
 }
-
